2f8a572f68ba0131d52ba991ef63a15deeda4555
Felipe N. Moura <felipenmoura@gmail.com>
2016-10-16 12:59:42 -0200

    Added layout and behavior

--- scripts/default.js
+++ scripts/default.js
@@ -1,12 +1,19 @@
 (function (){
     const originalVibrate = navigator.vibrate;
     var timeout = null;
+    var _b = document.body || false;
+    var enabled = true;
     
     navigator.vibrate = function(){
         
         var timer = 0;
-        var _b = document.body || false;
         var curToggleStatus = true;
+        
+        if (!enabled) {
+            return false;
+        }
+        
+        enabled = false;
 
         function toggle (force) {
             if(!_b) return;
@@ -22,7 +29,10 @@
             timeout = setTimeout(toggle, timer);
             timer+= cur;
         });
-        setTimeout(function(){toggle(true);}, ++timer);
+        setTimeout(function(){
+            toggle(true);
+            enabled = true;
+        }, ++timer);
         return originalVibrate.apply(navigator, arguments);
     };
     
@@ -35,6 +45,7 @@
         finalVib = [];
         start = 0;
         clearTimeout(timeout);
+        _b.classList.remove('recording');
     });
     document.getElementById('vib-button').addEventListener('click', function () {
         window.navigator.vibrate(finalVib.length? finalVib : [
@@ -57,9 +68,16 @@
         var d = new Date();
         var end = d.getTime();
         finalVib.push(end - start);
-        result.innerHTML= finalVib.join(', ');
+        result.innerHTML= finalVib.map(function(cur, idx, arr){
+            return idx%4===0? '<br/>&nbsp;&nbsp;' + cur: cur;
+        });
         start = d.getTime();
     }
+    
+    document.getElementById('start-recording-btn').addEventListener('click', function () {
+        enabled = !enabled;
+        _b.classList.toggle('recording');
+    });
     
     document.getElementById('listen-button').addEventListener('mousedown', down);
     document.getElementById('listen-button').addEventListener('mouseup', up);
